4e80a70d599ddf363c6ccaa0fb3add71ad32f13a
Kristoff Misquitta <kristoff.misquitta@gmail.com>
2022-01-18 21:56:29 -0800

    New tour class, code restructuring

--- test.py
+++ test.py
@@ -1,11 +1,10 @@
-from geopandas.tools.sjoin import _nearest_query
 from student_base import student_base
 import time
 import numpy as np
 import json
 import math
 import geopandas as gpd
-from shapely.geometry import Point, LineString, Polygon
+from shapely.geometry import Point
 from shapely.ops import nearest_points
 
 class Fire:
@@ -44,6 +43,49 @@ class Fire:
 		dist = math.sqrt((drone_x - fire_x)**2 + (drone_y - fire_y)**2)
 		return dist
 
+class Tour():
+	def __init__(self, start):
+		self.start = start
+		self.water = gpd.read_file('./data/waterbodies.geojson')['geometry']
+		self.fires = self._load_fires()
+	
+	def _load_fires(self):
+		f = open('maps/boston_fire.json')
+		data = json.load(f)
+		lats = data['data_fs']['ys']
+		lons = data['data_fs']['xs']
+		return [Fire(lats[i], lons[i]) for i in range(len(lats))]
+
+	def closest_fire(self, pos):
+		"""
+		Returns Fire closest to pos.
+		"""
+		return min(self.fires, key=lambda f: f.distance_to(pos))
+	
+	def closest_water(self, pos):
+		"""
+		Returns water closest to pos.
+		"""
+		pos = Point(*pos[::-1])
+		points = self.water.apply(lambda x: nearest_points(x,pos)[0])
+		closest_point = min(points, key=lambda x: pos.distance(x))
+		return (closest_point.y, closest_point.x)
+
+	def gen_tour(self):
+		"""
+		Generates a nearest neighbors tour through all fires on map.
+		"""
+		traversal = []
+		pos = self.start
+		for _ in range(len(self.fires)):
+			pos = self.closest_water(pos)
+			traversal.append((pos, 'water'))
+			next_fire = self.closest_fire(pos)
+			self.fires.remove(next_fire)
+			pos = next_fire.centroid
+			traversal.append((pos, 'fire'))
+		return traversal
+
 class my_flight_controller(student_base):
 	"""
 	Student flight controller class.
@@ -57,140 +99,47 @@ class my_flight_controller(student_base):
 	student_run(self, telemetry: Dict, commands: Dict (optional))
 		Method that takes in telemetry and issues drone commands.
 	"""
-	#CONSTANTS
-	FIRE_MAP = 'maps/boston_fire.json'
-	ALT = 100
-	
-	#STORAGE
-	fires = []	
-	water = gpd.read_file('./data/waterbodies.geojson')['geometry']
-
-	#LOCATIONS
-	anchor = None
-	home = None 	
-	
-	def get_lon_coords(self, path):
-		"""
-		Returns a list of the longitude coordinates of the fires from the map's json file.
-		"""
-		f = open(path)
-		data = json.load(f)
-		lon_coords = data['data_fs']['xs']
-		return [_ for _ in lon_coords]
-		
-	def get_lat_coords(self, path):
-		"""
-		Returns a list of the latitude coordinates of the fires from the map's json file.
-		"""
-		f = open(path)
-		data = json.load(f)
-		lat_coords = data['data_fs']['ys']
-		return [_ for _ in lat_coords]
-
-	def add_fires(self):
-		"""
-		Adds all fires on map to the fires field.
-		"""
-		lats = self.get_lat_coords(self.FIRE_MAP)
-		lons = self.get_lon_coords(self.FIRE_MAP)
-		my_flight_controller.fires = [Fire(lats[i], lons[i]) for i in range(len(lats))]
-	
-	def closest_fire(self, pos):
-		"""
-		Returns Fire closest to pos.
-		"""
-		return min(my_flight_controller.fires, key=lambda f: f.distance_to(pos))
-	
-	def closest_water(self, pos):
-		"""
-		Returns closest water to pos.
-		"""
-		pos = Point(*pos[::-1])
-		points = self.water.apply(lambda x: nearest_points(x,pos)[0])
-		closest_point = min(points, key=lambda x: pos.distance(x))
-		print('closest',closest_point)
-		return (closest_point.y, closest_point.x)
-
-	def student_run(self, telemetry, commands):
+	start = None
+	def student_run(self, telemetry, commands):			
 		#~~~~~~~~~SETUP & MOTION~~~~~~~~~
 		def setup():
-			self.add_fires()
 			print("Printing telemetry")
+			time.sleep(1)
+			print(telemetry)
 			
-			for i in range(4):
-				print(telemetry)
-				time.sleep(0.5)
-			
-			print("Waiting 6 seconds")
-			time.sleep(6)
+			print("Waiting 3 seconds")
+			time.sleep(3)
 
 			print("Arming")
 			self.arm()
 			print("Taking off")
-			self.home = self.anchor = (telemetry['latitude'], telemetry['longitude'])
-			#print('home', self.home)
+			self.start = (telemetry['latitude'], telemetry['longitude'])
 			self.takeoff()
 
-		def tear_down():
-			move_to(self.home, 'base')
-			time.sleep(10)
-			print("Landing"), self.land()
-			
-			while telemetry['in_air']:
-				time.sleep(0.1)
-				
-			print("Landed")
-			while True:
-				lat = telemetry['latitude']
-				lon = telemetry['longitude']
-				print(f'Latitude: {lat}, Longitude: {lon}')
-				time.sleep(1)
-
-		def move_to(pos, label):
-			lat, lon = pos
-			print(f'Get to {label}')
-			self.goto(lat, lon, 100)
-			err = np.linalg.norm([lat - telemetry['latitude'], lon - telemetry['longitude']])
-			tol = 0.0001
-			while err > tol:
-				print(f'Aircraft enroute to {label}')
-				time.sleep(10)
+		def execute_tour(tour):
+			for pt in tour:
+				lat, lon = pt[0]
+				print(f'Get to {pt[1]}')
+				self.goto(lat, lon, 100)
+				tol = 0.0001
 				err = np.linalg.norm([lat - telemetry['latitude'], lon - telemetry['longitude']])
-			self.anchor = (lat, lon)
-			print(f'Arrived at {label}')
-
+				while err > tol:
+					print(f'Aircraft enroute to {pt[1]}')
+					time.sleep(10)
+					err = np.linalg.norm([lat - telemetry['latitude'], lon - telemetry['longitude']])
+				print(f'Arrived at {pt[1]}')
+				time.sleep(10)
 
 		#~~~~~~~~COMMAND SEQUENCE~~~~~~~~
 		setup()
-		for _ in range(len(self.fires)):
-			move_to(self.closest_water(self.anchor), 'water')
-			time.sleep(6)
-			next_fire = self.closest_fire(self.anchor)
-			self.fires.remove(next_fire)
-			time.sleep(6)
-			move_to(next_fire.centroid, 'fire')
-			time.sleep(6)
-		tear_down()
+		execute_tour(Tour(self.start).gen_tour())
+		
+		print("Landing"), self.land()
+		while telemetry['in_air']:
+			time.sleep(0.1)		
+		print("Landed")
 
 if __name__ == "__main__":
 	fcs = my_flight_controller()
 	fcs.run()
-
-	# a = Point(-70.9899999, 42.3599999)
-	# choose_point(a)
-
-	# print(d)
-	# print(
-	# 	nearest_points(d,a)[0]
-	# )
-	
-	# a = LineString([Point(-78.929596, 40.349718), Point(-78.934682, 40.360541), Point(-78.965688, 40.388129)])
-	# b = Point(-78.96, 40.36)
-	# print(
-	# 	nearest_points(a,b)[0]
-	# )
-	# a = shapely.geometry.LineString([shapely.geometry.Point(0,0), shapely.geometry.Point(1,0)])
-	# b = shapely.geometry.Point(0.5,1)
-
-	# print(shapely.ops.nearest_points(a,b)[0]) # POINT (0.5, 0)
 	
